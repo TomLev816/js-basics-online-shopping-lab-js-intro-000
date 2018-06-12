709dc340cd3b68910f2db53a7e94dbc450f6058b
Thomas Lever <thomaslever508@gmail.com>
2018-06-12 17:44:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push(Object.assign({} , {itemName: item}, {itemPrice: Math.floor(Math.random() * Math.floor(100))}))
+ return `${item} has been added to your cart.`
 }
 
-function viewCart() {
-  // write your code here
+function viewCart(){
+  if (cart.length === 0){
+    return 'Your shopping cart is empty.'
+  } else {
+    let theCart = []
+    for (let i = 0; i < cart.length; i++){
+      theCart.push(`${cart[i].itemName} at ${cart[i].itemPrice}`)
+    }
+return 'The line is currently:' + theCart
+  }
 }
+
 
 function total() {
   // write your code here
